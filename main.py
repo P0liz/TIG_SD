Fixed individual export method and changed confidence calculation

main.py
@@ -6,7 +6,7 @@ from predictor import Predictor
 from digit_mutator import DigitMutator
 from mnist_member import MnistMember
 
-def main(prompt, expected_label, max_steps=100):
+def main(prompt, expected_label, max_steps=50):
 
     # Starting from a random latent noise vector
     latent = torch.randn(1, 4, 64, 64, dtype=torch.float32)

mutation_manager.py
@@ -32,8 +32,8 @@ class SDPipelineManager:
         print("Loading Stable Diffusion pipeline...")
         
         # Determine dtype based on device
-        dtype = torch.float16 if DEVICE == "cuda:0" else torch.float32
-        variant = "fp16" if dtype == torch.float16 else None
+        dtype = torch.float16 #if DEVICE == "cuda:0" else torch.float32
+        variant = "fp16" #if dtype == torch.float16 else None
         
         # Load pipeline
         self._pipe = StableDiffusionPipeline.from_pretrained(

predictor.py
@@ -27,9 +27,15 @@ class Predictor:
         
         # TODO: not really a confidence score, just appling softmax to logits
         # but this does not account for cases when all logits are similar (model is unsure)
-        probs = F.softmax(torch.tensor(original_logit), dim=0)
-        confidence = probs[original_label].item()
+        #probs = F.softmax(torch.tensor(original_logit), dim=0)
+        #confidence = probs[original_label].item()
+        confidence = Predictor.confidence_margin(torch.tensor(original_logit))
         return original_label, confidence
+
+    def confidence_margin(logits):
+        sorted_logits, _ = torch.sort(logits, descending=True)
+        margin = sorted_logits[0] - sorted_logits[1]
+        return margin.item()
 
     """ TODO: understand if needed
     @staticmethod